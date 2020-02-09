585f3b07e7462fb39d7638e4138d6eabf8b47c9f
Harsh Rana <hr23232323@gmail.com>
2020-02-09 15:05:52 -0500

    seeking alpha news reader

--- news_scraper.py
+++ news_scraper.py
@@ -8,10 +8,22 @@ eastern = pytz.timezone('US/Eastern')
 
 def main():
     stock_to_pull = "RDFN"
-    base_url = "https://www.tiingo.com/"+stock_to_pull+"/overview"
+    base_url = "https://seekingalpha.com/symbol/"+stock_to_pull
     res = requests.get(base_url)
     soup = BeautifulSoup(res.text, features="lxml")
-    print(res)
+
+    article_list = soup.find("ul", {"id": "symbol-page-latest"})
+    articles = sv.select('li', article_list)
+    news_df = pd.DataFrame()
+    for article in articles:
+        news_det = {}
+        news_det["headline"] = article.find("div", {"class" : "symbol_article"}).text
+        spans = article.find("div", {"class" : "date_on_by"}).findChildren("span", {"class": ""})
+        for span in spans:
+            
+        #news_det["release_dt"] = article.find("div", {"class" : "date_on_by"}).text
+        #print(news_det)
+        print('\n\n')
 
 
 if __name__ == "__main__":
