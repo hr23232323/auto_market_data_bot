a68864e4e8e441f12dd81a8bc7fcfd2273c28bc0
Harsh Rana <hr23232323@gmail.com>
2020-02-09 14:42:12 -0500

    news scraper started

--- news_scraper.py
+++ news_scraper.py
@@ -8,9 +8,10 @@ eastern = pytz.timezone('US/Eastern')
 
 def main():
     stock_to_pull = "RDFN"
-    base_url = "https://finance.yahoo.com/quote/"+stock_to_pull+"/options?p="+stock_to_pull+"&straddle=false"
+    base_url = "https://www.tiingo.com/"+stock_to_pull+"/overview"
     res = requests.get(base_url)
     soup = BeautifulSoup(res.text, features="lxml")
+    print(res)
 
 
 if __name__ == "__main__":
