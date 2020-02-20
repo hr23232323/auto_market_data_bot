59680230cd900ca95013b727aee9aee53c45a1b9
Harsh Rana <hr23232323@gmail.com>
2020-02-19 21:53:02 -0500

    stock list reader feature adde

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from technicals_scraper import scrape as ts
 
 
 def main():
+    stock_list_txt = open("stock_list.txt", 'r')
+    stock_list = [line.rstrip('\n').split(',') for line in stock_list_txt.readlines()]
+    
     ts("rdfn")
 
 
