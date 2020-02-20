889052262ac539084a38d1c0067d7d0ac7c66d9f
Harsh Rana <hr23232323@gmail.com>
2020-02-19 22:07:44 -0500

    other scrapers added to main

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import time
 from technicals_scraper import scrape as ts
-
+from calls_scraper import scrape as cs
+from puts_scraper import scrape as ps
 
 def main():
+    # text stock list to array
     stock_list_txt = open("stock_list.txt", 'r')
     stock_list = []
     with open('stock_list.txt','r') as f:
         for line in f:
             for word in line.rstrip('\n').split(','):
                stock_list.append(word)
-    print(stock_list)
+
+    # scrape for each stock
     for stock in stock_list:
         print(stock)
-        ts(stock)
-
-
-
-
-
+        #ts(stock)
 
 
         time.sleep(1)
