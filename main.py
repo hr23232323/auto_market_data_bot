c045ef591486537796ae978d342e3582318f41a3
Harsh Rana <hr23232323@gmail.com>
2020-02-19 22:01:51 -0500

    text list to array added

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import time
 from technicals_scraper import scrape as ts
 
 
 def main():
     stock_list_txt = open("stock_list.txt", 'r')
-    stock_list = [line.rstrip('\n').split(',') for line in stock_list_txt.readlines()]
-    
-    ts("rdfn")
+    stock_list = []
+    with open('stock_list.txt','r') as f:
+        for line in f:
+            for word in line.rstrip('\n').split(','):
+               stock_list.append(word)
+    print(stock_list)
+    for stock in stock_list:
+        print(stock)
+        ts(stock)
+
+
+
+
+
+
+
+        time.sleep(1)
+
 
 
 
