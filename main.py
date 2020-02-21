a7b94604c56ffd85aad1c94cd93c21ec829649bd
Harsh Rana <hr23232323@gmail.com>
2020-02-20 22:05:41 -0500

    scrape time randomized to mimic human behavior

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from technicals_scraper import scrape as ts
 from calls_scraper import scrape as cs
 from puts_scraper import scrape as ps
 from excel_writer import append_df_to_excel
+import random
 
 def main():
     # text stock list to array
@@ -16,24 +17,24 @@ def main():
 
     # scrape for each stock
     for stock in stock_list:
-        print(stock)
         technicals_df = ts(stock)
-        time.sleep(1)
+        time.sleep(random.uniform(0.5, 1.5))
         puts_df = ps(stock)
-        time.sleep(1)
+        time.sleep(random.uniform(0.5, 1.5))
         calls_df = cs(stock)
-        time.sleep(1)
-        break
+        time.sleep(random.uniform(0.5, 1.5))
 
-    # write to CSV/DB. Only write headers if first time
-    if(path.exists("database_text.xlsx")):
-        append_df_to_excel('database_text.xlsx', technicals_df, sheet_name='technicals', index=False, header=False)
-        append_df_to_excel('database_text.xlsx', puts_df, sheet_name='puts', index=False, header=False)
-        append_df_to_excel('database_text.xlsx', calls_df, sheet_name='calls', index=False, header=False)
-    else:
-        append_df_to_excel('database_text.xlsx', technicals_df, sheet_name='technicals', index=False)
-        append_df_to_excel('database_text.xlsx', puts_df, sheet_name='puts', index=False)
-        append_df_to_excel('database_text.xlsx', calls_df, sheet_name='calls', index=False)
+        # write to CSV/DB. Only write headers if first time
+        if(path.exists("database_text.xlsx")):
+            append_df_to_excel('database_text.xlsx', technicals_df, sheet_name='technicals' + stock, index=False, header=False)
+            append_df_to_excel('database_text.xlsx', puts_df, sheet_name='puts' + stock, index=False, header=False)
+            append_df_to_excel('database_text.xlsx', calls_df, sheet_name='calls' + stock, index=False, header=False)
+        else:
+            append_df_to_excel('database_text.xlsx', technicals_df, sheet_name='technicals' + stock, index=False)
+            append_df_to_excel('database_text.xlsx', puts_df, sheet_name='puts' + stock, index=False)
+            append_df_to_excel('database_text.xlsx', calls_df, sheet_name='calls' + stock, index=False)
+
+        break
 
 
 if __name__ == "__main__":
