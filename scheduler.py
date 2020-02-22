f63f0cad9c4eb8b88f46d16c34c8c1c6e4717b95
Harsh Rana <hr23232323@gmail.com>
2020-02-22 16:52:50 -0500

    scheduling attempt 2

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,11 @@
 import datetime
 from datetime import datetime as dt
 import schedule
-
-
+import time
 
 def time_is_between(min_time, max_time, to_check_time):
     if(to_check_time > min_time and to_check_time < max_time):
+        print(to_check_time)
         print("YES")
     else:
         print("NO")
@@ -14,12 +14,12 @@ def collect_data():
     print(dt.now())
     print("COLLECTING DATA")
 
-def start_day():
-    schedule.every(1).minutes.do(collect_data)
+def start_day(market_open, market_close):
+    schedule.every(10).seconds.do(collect_data)
 
     # when to stop
-    market_open = datetime.time(9,30,00)
-    market_close = datetime.time(17,00,00)
+    current_time = dt.now().time()
+
     while (time_is_between(market_open, market_close, current_time)):
         schedule.run_pending()
         time.sleep(1)
@@ -27,7 +27,15 @@ def start_day():
 
 
 def main():
-    schedule.
+    market_open = datetime.time(9,30,00)
+    market_close = datetime.time(16,52,00)
+    current_time = dt.now().time()
+    while 1:
+        if(time_is_between(market_open, market_close, current_time)):
+            start_day(market_open, market_close)
+            time.sleep(180)
+        time.sleep(1)
+        current_time = dt.now().time()
 
 
 
