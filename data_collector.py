57895ee3d76a565d49c8a1e25338164141488ddb
Harsh Rana <hr23232323@gmail.com>
2020-02-22 19:33:23 -0500

    scheduler tested

---
+++ data_collector.py
@@ -0,0 +1,34 @@
+import time
+from os import path
+from technicals_scraper import scrape as ts
+from calls_scraper import scrape as cs
+from puts_scraper import scrape as ps
+from excel_writer import append_df_to_excel
+import random
+
+def collect_data():
+    # text stock list to array
+    print("COLLECTING DATA")
+    test = True
+    stock_list_txt = open("stock_list.txt", 'r')
+    stock_list = []
+    with open('stock_list.txt','r') as f:
+        for line in f:
+            for word in line.rstrip('\n').split(','):
+               stock_list.append(word)
+
+    # scrape for each stock
+    if(test):
+        stock_list = ['RDFN']
+    for stock in stock_list:
+        technicals_df = ts(stock)
+        time.sleep(random.uniform(0.5, 1.5))
+        puts_df = ps(stock)
+        time.sleep(random.uniform(0.5, 1.5))
+        calls_df = cs(stock)
+        time.sleep(random.uniform(0.5, 1.5))
+
+        # write to CSV/DB. Only write headers if first time
+        append_df_to_excel('database_text.xlsx', technicals_df, sheet_name=stock + '_technicals', index=False)
+        append_df_to_excel('database_text.xlsx', puts_df, sheet_name=stock + '_puts', index=False)
+        append_df_to_excel('database_text.xlsx', calls_df, sheet_name=stock + '_calls', index=False)
--- main.py
+++ main.py
@@ -1,37 +1,38 @@
+import datetime
+from datetime import datetime as dt
+import schedule
 import time
-from os import path
-from technicals_scraper import scrape as ts
-from calls_scraper import scrape as cs
-from puts_scraper import scrape as ps
-from excel_writer import append_df_to_excel
-import random
+from data_collector import collect_data
+
+def time_is_between(min_time, max_time, to_check_time):
+    if(to_check_time > min_time and to_check_time < max_time):
+        return True
+    else:
+        return False
+
+
+def start_day(market_open, market_close):
+    print("MARKET STARTED")
+    schedule.every(10).seconds.do(collect_data)
+
+    # when to stop
+    current_time = dt.now().time()
+    while (time_is_between(market_open, market_close, current_time)):
+        schedule.run_pending()
+        time.sleep(1)
+        current_time = dt.now().time()
+
 
 def main():
-    # text stock list to array
-    test = True
-    stock_list_txt = open("stock_list.txt", 'r')
-    stock_list = []
-    with open('stock_list.txt','r') as f:
-        for line in f:
-            for word in line.rstrip('\n').split(','):
-               stock_list.append(word)
-
-    # scrape for each stock
-    if(test):
-        stock_list = ['RDFN']
-    for stock in stock_list:
-        technicals_df = ts(stock)
-        time.sleep(random.uniform(0.5, 1.5))
-        puts_df = ps(stock)
-        time.sleep(random.uniform(0.5, 1.5))
-        calls_df = cs(stock)
-        time.sleep(random.uniform(0.5, 1.5))
-
-        # write to CSV/DB. Only write headers if first time
-        append_df_to_excel('database_text.xlsx', technicals_df, sheet_name=stock + '_technicals', index=False)
-        append_df_to_excel('database_text.xlsx', puts_df, sheet_name=stock + '_puts', index=False)
-        append_df_to_excel('database_text.xlsx', calls_df, sheet_name=stock + '_calls', index=False)
-
+    market_open = datetime.time(9,30,00)
+    market_close = datetime.time(19,28,00)
+    current_time = dt.now().time()
+    while 1:
+        if(time_is_between(market_open, market_close, current_time)):
+            start_day(market_open, market_close)
+            time.sleep(86340)
+        time.sleep(1)
+        current_time = dt.now().time()
 
 
 if __name__ == "__main__":
