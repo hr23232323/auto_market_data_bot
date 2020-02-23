30c516467ef64852c739b8900100c4f1d8a39f2a
Harsh Rana <hr23232323@gmail.com>
2020-02-22 19:21:47 -0500

    scheduler finished

--- scheduler.py
+++ scheduler.py
@@ -7,14 +7,17 @@ def time_is_between(min_time, max_time, to_check_time):
     if(to_check_time > min_time and to_check_time < max_time):
         print(to_check_time)
         print("YES")
+        return True
     else:
         print("NO")
+        return False
 
 def collect_data():
     print(dt.now())
     print("COLLECTING DATA")
 
 def start_day(market_open, market_close):
+    print("MARKET STARTED")
     schedule.every(10).seconds.do(collect_data)
 
     # when to stop
@@ -28,12 +31,13 @@ def start_day(market_open, market_close):
 
 def main():
     market_open = datetime.time(9,30,00)
-    market_close = datetime.time(16,52,00)
+    market_close = datetime.time(16,00,00)
     current_time = dt.now().time()
+    print(time_is_between(market_open, market_close, current_time))
     while 1:
         if(time_is_between(market_open, market_close, current_time)):
             start_day(market_open, market_close)
-            time.sleep(180)
+            time.sleep(86340)
         time.sleep(1)
         current_time = dt.now().time()
 
